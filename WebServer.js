b1f7677711cf561e187606aeac05178eff3847f9
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2024-01-21 08:46:41 -0300

    Dejar de rastrear 'node_modules'

--- WebServer.js
+++ WebServer.js
@@ -1,20 +1,45 @@
 console.clear();
-import createExpressServer from 'express';
+import express from 'express';
 
 const PORT = 3000;
 
-const expressApp = createExpressServer()
+const expressApp = express()
 
 
-expressApp.all("/cuenta/:idcuenta", (req,res)=>{
-    console.log(req.params.idcuenta); //devuelve el id de lo que se pidio y si se contantena son objetos
-    //req.headers
-    //req.get
-    //res.send("Tu cuenta personal"); la respuesta
-    // res.status(401).send({
-    //     errorMessage: "No autorizado"
-    // });
-})
+//-------------Middlewares-------------------// 
+expressApp.use(express.json())   //Parsea las solicitudes a json (lo que venga a json)
+expressApp.use(express.text()) //Convierte lo que venga a texto 
+///---------------------------------------------------------------
+
+
+// expressApp.post("/cuenta/:idcuenta", (req,res)=>{
+//     //console.log(req.params.idcuenta); //devuelve el id de lo que se pidio y si se contantena son objetos
+//     //req.headers
+//     //req.get
+//     //res.send("Tu cuenta personal"); la respuesta
+//     // res.status(401).send({
+//     //     errorMessage: "No autorizado"
+//     //console.log(req.query); //Muestra los parametros de la queery como un objeto
+//     // });
+//     console.log(req.body);
+//     res.send();
+
+// })
+
+// expressApp.put("/producto/:idcuenta",(req,res)=>{
+//     console.log(req.body);
+//     res.send();
+// });
+
+
+//obtener
+
+//crear
+
+//actualizar
+
+//eliminar
+
 
 expressApp.listen(PORT,()=>
 console.log(`servidor levantado en el puerto ${PORT}`));
